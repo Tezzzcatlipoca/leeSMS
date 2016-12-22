85972cbb2da71e4d877c3e8c42b037e50310e7a7
Franco <robertoe.francoa@gmail.com>
2016-12-22 14:01:05 -0600

    Agrega funcion para buscar en index_period_source y source_master al mismo tiempo.

--- R/extraeSMS.R
+++ R/extraeSMS.R
@@ -169,3 +169,45 @@ sm<-function(variables,args=NA){
      close(sms)
      out
 }
+
+sm_ips<-function(index_id,variables,args=NA){
+     # Extrae de IPS
+     smsh<-odbcConnect('smsh','nretail','nretail')
+     per<-ult.periodo(index_id)
+     varos<-paste0(variables,collapse = ", ")
+     if (!is.na(args)) {
+          linea<-paste0("SELECT * FROM index_period_source WHERE status_id IN (6,7,8,9) AND index_id = ",index_id," AND period_id = ",per," AND ",args)
+     } else {
+          linea<-paste0("SELECT * FROM index_period_source wHERE status_id IN (6,7,8,9) AND index_id = ",index_id," AND period_id = ",per)
+     }
+     out_ips<-sqlQuery(smsh,linea)
+     close(smsh)
+
+     # Extrae de SM
+     sms<-odbcConnect('sms','nretail','nretail')
+     tiendas<-paste0(out_ips$source_id,collapse = ",")
+     linea2<-paste0("SELECT * FROM source_master WHERE source_id IN (",tiendas,")")
+     out_sm<-sqlQuery(sms,linea2)
+     close(sms)
+
+     # Une ambas tablas
+     out_total<-merge(out_ips,out_sm,by="source_id",all.x=T)
+     variables<-gsub(" ","",variables) # Elimina espacios en blanco
+     vars<-as.character(strsplit(gsub(" ","",variables),",")[[1]]) # Extrae cada variable del input del usuario
+
+     # Extraer variables buscadas
+     found<-names(out_total) %in% vars
+     corrected1<-paste0(vars,".x")
+     corrected2<-paste0(vars,".y")
+     found1<-which(names(out_total) %in% corrected1)
+     found2<-which(names(out_total) %in% corrected2)
+     found_tot<-c(found1,found2)
+     found[found_tot]<-TRUE
+     if(variables=="*"){
+          out<-out_total
+     } else {
+          out<-out_total[,which(found)]
+     }
+     out
+}
+
